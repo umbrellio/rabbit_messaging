4ce72dd222dfd7875197ee91bed61caae43fa719
VegetableProphet <slepoy.homyachok@gmail.com>
2020-10-28 17:06:59 +0300

    version down

--- lib/rabbit/receiving/job.rb
+++ lib/rabbit/receiving/job.rb
@@ -11,12 +11,14 @@ require "rabbit/receiving/malformed_message"
 class Rabbit::Receiving::Job < ActiveJob::Base
   def perform(message, arguments)
     Lamian.run do
-      message = Rabbit::Receiving::Message.build(message, arguments)
-      handler = Rabbit::Receiving::HandlerResolver.handler_for(message)
-      handler.new(message).call
-    rescue Rabbit::Receiving::MalformedMessage => error
-      raise if Rabbit.config.environment == :test
-      Rabbit.config.exception_notifier.call(error)
+      begin
+        message = Rabbit::Receiving::Message.build(message, arguments)
+        handler = Rabbit::Receiving::HandlerResolver.handler_for(message)
+        handler.new(message).call
+      rescue Rabbit::Receiving::MalformedMessage => error
+        raise if Rabbit.config.environment == :test
+        Rabbit.config.exception_notifier.call(error)
+      end
     end
   end
 end
