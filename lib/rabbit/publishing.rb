abe0ae3905f766c364a0b10491399a0270f2819c
johny <krychaw@inbox.lv>
2020-10-29 23:39:53 +0300

    delegate manage are channels to ChannelsPool

--- lib/rabbit/publishing.rb
+++ lib/rabbit/publishing.rb
@@ -5,39 +5,98 @@ require "rabbit/publishing/message"
 module Rabbit
   module Publishing
     autoload :Job, "rabbit/publishing/job"
+    extend self
 
     MUTEX = Mutex.new
 
-    extend self
+    class ChannelsPool
+      class BaseQueue < Queue
+        def initialize(session, max_size)
+          @session    = session
+          @max_size   = max_size
+          @ch_size    = 0
+          @create_mon = Mutex.new
+        end
 
-    def publish(message)
-      return unless client
+        def pop
+          create_channel if size == 0
 
-      channel = channel(message.confirm_select?)
-      channel.basic_publish(*message.basic_publish_args)
+          super
+        end
 
-      if message.confirm_select? && !channel.wait_for_confirms
-        raise MessageNotDelivered, "RabbitMQ message not delivered: #{message}"
-      else
-        log(message)
+        def push(ch)
+          return @ch_size -= 1 unless ch&.open?
+
+          super
+        end
+
+        def init_channel
+          @create_mon.synchronize do
+            return unless @ch_size < @max_size
+
+            push create_channel
+            @ch_size += 1
+
+            channel
+          end
+        end
+
+        private
+
+        def create_channel
+          @session.create_channel
+        end
+      end
+
+      class ConfirmQueue < BaseQueue
+        def create_channel
+          @session.create_channel.confirm_select
+        end
+      end
+
+      def initialize(session)
+        max_size = session.channel_max
+
+        @pools = {
+          true:  ConfirmQueue.new(session, max_size/2),
+          false: BaseQueue.new(session, max_size/2)
+        }.freeze
+      end
+
+      def with_channel(confirm)
+        pool = @pools[confirm]
+        ch = pool.pop
+        yield ch
+      ensure
+        pool.push ch
+      end
+    end
+
+    def publish(msg)
+      # return unless client
+
+      # channel = channel(message.confirm_select?)
+      # channel.basic_publish(*message.basic_publish_args)
+      return if Rabbit.config.environment.in? %i[test development]
+
+      pool.with_channel msg.confirm_select? do |ch|
+        ch.basic_publish *msg.basic_publish_args
+
+        raise MessageNotDelivered, "RabbitMQ message not delivered: #{msg}" \
+          if msg.confirm_select? && !ch.wait_for_confirms
+
+        log msg
       end
     rescue Timeout::Error
       raise MessageNotDelivered, <<~MESSAGE
-        Timeout while sending message #{message}. Possible reasons:
-          - #{message.real_exchange_name} exchange is not found
+        Timeout while sending message #{msg}. Possible reasons:
+          - #{msg.real_exchange_name} exchange is not found
           - RabbitMQ is extremely high loaded
       MESSAGE
     end
 
-    def client
-      MUTEX.synchronize { @client ||= create_client }
-    end
-
-    def channel(confirm)
-      Thread.current[:bunny_channels] ||= {}
-      channel = Thread.current[:bunny_channels][confirm]
-      Thread.current[:bunny_channels][confirm] = create_channel(confirm) unless channel&.open?
-      Thread.current[:bunny_channels][confirm]
+    def pool
+      MUTEX.synchronize { @pool = ChannelsPool.new(create_client) }
     end
 
     private
@@ -47,22 +106,10 @@ module Rabbit
     end
 
     def create_client
-      return if Rabbit.config.environment == :test
-
       config = Rails.application.config_for("sneakers") rescue {}
       config = config["bunny_options"].to_h.symbolize_keys
 
-      begin
-        Bunny.new(config).start
-      rescue
-        raise unless Rabbit.config.environment == :development
-      end
-    end
-
-    def create_channel(confirm)
-      channel = client.create_channel
-      channel.confirm_select if confirm
-      channel
+      Bunny.new(config).start
     end
 
     def log(message)
