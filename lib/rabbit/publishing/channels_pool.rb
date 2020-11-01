dc76eef4f5a3959847192feac0dfee91079d7755
johny <krychaw@inbox.lv>
2020-11-01 14:57:33 +0300

    fix linter admonitions

--- lib/rabbit/publishing/channels_pool.rb
+++ lib/rabbit/publishing/channels_pool.rb
@@ -1,65 +1,67 @@
-    class ChannelsPool
-      class BaseQueue < Queue
-        def initialize(session, max_size)
-          super()
+# frozen_string_literal: true
 
-          @session    = session
-          @max_size   = max_size - 1
-          @ch_size    = 0
-          @create_mon = Mutex.new
-        end
+class ChannelsPool
+  class BaseQueue < Queue
+    def initialize(session, max_size)
+      super()
 
-        def pop
-          add_channel if size == 0
+      @session    = session
+      @max_size   = max_size - 1
+      @ch_size    = 0
+      @create_mon = Mutex.new
+    end
 
-          super
-        end
+    def pop
+      add_channel if size.zero?
 
-        def push(ch)
-          return @ch_size -= 1 unless ch&.open?
+      super
+    end
 
-          super
-        end
+    def push(ch)
+      return @ch_size -= 1 unless ch&.open?
 
-        def add_channel
-          @create_mon.synchronize do
-            return unless @ch_size < @max_size
+      super
+    end
 
-            push create_channel
-            @ch_size += 1
-          end
-        end
+    def add_channel
+      @create_mon.synchronize do
+        return unless @ch_size < @max_size
 
-        protected
-
-        def create_channel
-          @session.create_channel
-        end
-      end
-
-      class ConfirmQueue < BaseQueue
-        def create_channel
-          ch = @session.create_channel
-          ch.confirm_select
-
-          ch
-        end
-      end
-
-      def initialize(session)
-        max_size = session.channel_max
-
-        @pools = {
-          true  => ConfirmQueue.new(session, max_size/2),
-          false => BaseQueue.new(session, max_size/2)
-        }.freeze
-      end
-
-      def with_channel(confirm)
-        pool = @pools[confirm]
-        ch = pool.pop
-        yield ch
-      ensure
-        pool.push ch
+        push create_channel
+        @ch_size += 1
       end
     end
+
+    protected
+
+    def create_channel
+      @session.create_channel
+    end
+  end
+
+  class ConfirmQueue < BaseQueue
+    def create_channel
+      ch = @session.create_channel
+      ch.confirm_select
+
+      ch
+    end
+  end
+
+  def initialize(session)
+    max_size = session.channel_max
+
+    @pools = {
+      true  => ConfirmQueue.new(session, max_size / 2),
+      false => BaseQueue.new(session, max_size / 2),
+    }.freeze
+  end
+
+  def with_channel(confirm)
+    pool = @pools[confirm]
+    ch = pool.pop
+    yield ch
+  ensure
+    pool.push ch
+  end
+end
