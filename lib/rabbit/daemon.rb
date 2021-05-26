f04ed1aa2a572a4904f1f66199522dcb3b76a3de
Yuri Smirnov <tycooon@yandex.ru>
2021-05-26 18:15:47 +0300

    minor refactor

--- lib/rabbit/daemon.rb
+++ lib/rabbit/daemon.rb
@@ -18,24 +18,11 @@ module Rabbit
         Lamian.extend_logger(logger)
       end
 
-      Sneakers.configure(
-        connection: connection,
-        env: Rails.env,
-        exchange_type: :direct,
-        exchange: Rabbit.config.app_name,
-        hooks: Rabbit.config.hooks,
-        supervisor: true,
-        daemonize: false,
-        exit_on_detach: true,
-        log: logger,
-        **config,
-      )
-
+      Sneakers.configure(**sneakers_config(logger: logger))
       Sneakers.server = true
 
       Rabbit.config.validate!
       Receiving::Worker.from_queue(Rabbit.config.read_queue)
-
       Sneakers::Runner.new([Receiving::Worker]).run
     end
 
@@ -46,6 +33,23 @@ module Rabbit
     def connection
       bunny_config = config.delete(:bunny_options).to_h.symbolize_keys
       Bunny.new(bunny_config)
+    end
+
+    private
+
+    def sneakers_config(logger:)
+      {
+        connection: connection,
+        env: Rails.env,
+        exchange_type: :direct,
+        exchange: Rabbit.config.app_name,
+        hooks: Rabbit.config.hooks,
+        supervisor: true,
+        daemonize: false,
+        exit_on_detach: true,
+        log: logger,
+        **config,
+      }
     end
   end
 end
