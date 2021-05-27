e3a243bda74dfcba792d521050bffc454e5c944b
Yuri Smirnov <tycooon@yandex.ru>
2021-05-27 19:59:28 +0300

    Fix sneakers logger (#10)

--- lib/rabbit/daemon.rb
+++ lib/rabbit/daemon.rb
@@ -11,30 +11,18 @@ module Rabbit
   module Daemon
     extend self
 
-    def run
-      Sneakers.configure(
-        connection: connection,
-        env: Rails.env,
-        exchange_type: :direct,
-        exchange: Rabbit.config.app_name,
-        hooks: Rabbit.config.hooks,
-        supervisor: true,
-        daemonize: false,
-        exit_on_detach: true,
-        **config,
-      )
-
-      unless Sneakers.logger
-        Sneakers.logger = Logger.new(Rails.root.join("log", "sneakers.log"))
-        Sneakers.logger.level = Logger::DEBUG
-        Lamian.extend_logger(Sneakers.logger)
+    def run(logger: Sneakers.logger)
+      unless logger
+        logger = Logger.new(Rails.root.join("log", "sneakers.log"))
+        logger.level = Logger::DEBUG
+        Lamian.extend_logger(logger)
       end
 
+      Sneakers.configure(**sneakers_config(logger: logger))
       Sneakers.server = true
 
       Rabbit.config.validate!
       Receiving::Worker.from_queue(Rabbit.config.read_queue)
-
       Sneakers::Runner.new([Receiving::Worker]).run
     end
 
@@ -45,6 +33,23 @@ module Rabbit
     def connection
       bunny_config = config.delete(:bunny_options).to_h.symbolize_keys
       Bunny.new(bunny_config)
+    end
+
+    private
+
+    def sneakers_config(logger:)
+      {
+        connection: connection,
+        env: Rails.env,
+        exchange_type: :direct,
+        exchange: Rabbit.config.app_name,
+        hooks: Rabbit.config.hooks,
+        supervisor: true,
+        daemonize: false,
+        exit_on_detach: true,
+        log: logger,
+        **config,
+      }
     end
   end
 end
